005227e624794aebb4e117686c6f174a07b1ec0c
Rudolph Venter <rudolph.rv@gmail.com>
2023-09-17 20:31:44 +0200

    fix: updated typings for data source files

--- src/DataSource/localFileHandler.ts
+++ src/DataSource/localFileHandler.ts
@@ -1,6 +1,6 @@
 import * as fsPromise from 'fs/promises';
 
-export async function readFile(path: string): Promise<string | undefined> {
+export async function readFile(path: string): Promise<string> {
     try {
         return fsPromise.readFile(path, 'utf-8').then((result) => result.trim());
     } catch (error) {
--- src/DataSource/studentDataHandler.ts
+++ src/DataSource/studentDataHandler.ts
@@ -1,7 +1,7 @@
 import { readFile } from './localFileHandler';
 
-export async function fetchStudentData(): Promise<string | undefined> {
-    return readFile('./src/testFile.txt').catch((rejectReason) => {
+export async function fetchStudentData(): Promise<string> {
+    return readFile('./src/sampleInput_1.txt').catch((rejectReason) => {
         console.error(`Failed to fetch student data`);
         throw rejectReason;
     });
